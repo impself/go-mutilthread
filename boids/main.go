18479a470fbbd8451bb8ff38a870ebf354248145
impself <774807698@qq.com>
2026-04-20 18:57:30 +0800

    main

---
+++ boids/main.go
@@ -0,0 +1,55 @@
+package boids
+
+type Vector2D struct {
+	x float64
+	y float64
+}
+
+func (v1 Vector2D) Add(v2 Vector2D) Vector2D {
+	return Vector2D{
+		v1.x + v2.x,
+		v1.y + v2.y,
+	}
+}
+
+func (v1 Vector2D) Substract(v2 Vector2D) Vector2D {
+	return Vector2D{
+		v1.x - v2.x,
+		v1.y - v2.y,
+	}
+}
+
+func (v1 Vector2D) Multiply(v2 Vector2D) Vector2D {
+	return Vector2D{
+		v1.x * v2.x,
+		v1.y * v2.y,
+	}
+}
+
+func (v1 Vector2D) AddV(d float64) Vector2D {
+	return Vector2D{
+		v1.x + d,
+		v1.y + d,
+	}
+}
+
+func (v1 Vector2D) MultiplyV(d float64) Vector2D {
+	return Vector2D{
+		v1.x * d,
+		v1.y * d,
+	}
+}
+
+func (v1 Vector2D) DivisionV(d float64) Vector2D {
+	return Vector2D{
+		v1.x / d,
+		v1.y / d,
+	}
+}
+
+func (v1 Vector2D)limit(lower,upper float64)Vector2D{
+	return Vector2D{
+		
+	}
+}
+
